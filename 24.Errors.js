bc2c919198daa98c8536afdd974f3b17e069d864
Madan.patakota <madan.patakota@gmail.com>
2025-09-09 09:34:29 +0530

    looping system

--- 24.Errors.js
+++ 24.Errors.js
@@ -1,13 +1,23 @@
 let age = 20;
 
-
 let sname;
 
-let uppercase = sname.toUpperCase();  // "JOHN";
 
-console.log(uppercase);
+    try
+    {
+        let uppercase = sname.toUpperCase();  // "JOHN";
+        console.log(uppercase);
+    }
+    catch(error){
+         console.log(error);
+        // console.log("Something is wrong .please check your input");
+    }
+    finally{
+        console.log("Finally its executed");
+    }
 
-console.log("Hey !!!! How are you ????")
+
+console.log("Hey !!!! How are you ????");
 
 //mobile shop 30k - mobile
 
---
+++ 26.Looping_mecanism.js
@@ -0,0 +1,78 @@
+
+
+// Try to understand
+// ----------------------- looping mecanism repeatly executes the your block of code until condition is gets false
+
+
+// Overview 
+
+
+// while 
+// do--while
+
+
+// for
+// for...in  -- to iterates over te properies of an object
+// for...of
+
+
+let rule = 0;
+
+while(rule < -1)
+{
+  console.log("Hello");
+  rule = rule + 1;
+}
+
+
+//------------------------------------------------------
+
+let index = 0
+do
+{
+  console.log("Index is " , index);
+  index = index + 1;
+}
+while(index < -1)
+
+//---------------------------------------------------
+
+
+
+
+
+// if(rule < 5 )
+// {
+//   console.log("Hello");
+//   rule = rule + 1;
+// }
+
+
+
+
+let student = { 
+                name: "Madan", 
+                course: "JS"  ,
+                location : "Bangolore"
+              };
+//repeating the object for the keys 
+//
+for(let abc in student){
+    console.log("key is " , abc);
+    console.log("now" , student[abc]);
+}
+
+            //   student.name
+            //   student[name]
+
+let fruits = ["Kiwi", "Mango", "grapes"];
+for(let fruit of fruits){
+    console.log(fruit);
+    if(fruit == "Kiwi"){
+         console.log("Kiwi price is " , 200);
+    }
+}
+
+
+
+//for()
